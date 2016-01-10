eb3d3631bceb580d17a1accf381cb025f68fbc89
jorcalle11 <jorcalle11@gmail.com>
2016-01-10 17:30:11 -0500

    se agrego el motor de plantillas jade

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var jade = require('jade');
+
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
-  res.send('hello express');
+  res.render('index');
 });
 
 app.listen(port, function(req,res){
   console.log('server running and listennig in http://localhost:'+port);
 });
+
