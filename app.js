8a6cc1fec419e560ee7804af05f7fdce0c7ed7c1
jorcalle11 <jorcalle11@gmail.com>
2016-01-11 00:37:02 -0500

    se creó el index y la seccion de smartphones

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
+
 app.get('/',function(req,res){
-  res.render('index');
+    res.render('index');
+});
+
+app.get('/smartphones',function(req,res){
+    res.render('smartphones');
 });
 
 app.listen(port, function(req,res){
-  console.log('server running and listennig in http://localhost:'+port);
+    console.log('server running and listennig in http://localhost:'+port);
 });
 
