b33259b60af5c1da9e0cada7030f4c54c58202e1
jorcalle11 <jorcalle11@gmail.com>
2016-01-12 16:52:24 -0500

    se agregó la sección de contacto y acerca de, además se adaptó para vista móvil

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var jade = require('jade');
-
+var morgan = require('morgan');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
+app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
@@ -13,6 +14,14 @@ app.get('/',function(req,res){
 
 app.get('/smartphones',function(req,res){
     res.render('smartphones');
+});
+
+app.get('/contact',function(req,res){
+	res.render('contact');
+});
+
+app.get('/about', function(req,res){
+	res.render('about');
 });
 
 app.listen(port, function(req,res){
