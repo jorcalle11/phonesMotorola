7bc5c28f1d15e50daa5ef5ac1c6a215dae40706d
jorcalle11 <jorcalle11@gmail.com>
2016-01-16 00:40:14 -0500

    se creó la vista para agregar un smartphone

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/',function(req,res){
     res.render('index');
 });
 
+router.get('/phones/new', function(req,res){
+	res.render('phones/new');
+})
+
 router.get('/smartphones',function(req,res){
     res.render('smartphones');
 });
